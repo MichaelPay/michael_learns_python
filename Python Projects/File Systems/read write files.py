ddf0af07d56fff1871d7777944abc907f3c6267c
Michael Pay <michael.pay@mail.com>
2017-08-23 15:39:00 -0700

    testing 123

---
+++ Python Projects/File Systems/read write files.py
@@ -0,0 +1,16 @@
+f = open('test.txt', 'r')
+
+print()
+
+f.close()
+
+# open and close files using context managers
+
+# with open('test.txt', 'r') as f:
+#     f_contents = f.read()
+#     print(f_contents)
+#
+# with open('test.txt', 'r') as f:
+#     f_contents = f.readlines()
+#     print(f_contents)
+
